124000de9ec9e56654b9ddaca32f680aaa321892
Shion Ichikawa <shion1305@gmail.com>
2024-03-13 01:52:54 +0900

    🐛 reader / writer のSignature修正

--- svc/pkg/infra/writer/milestone.go
+++ svc/pkg/infra/writer/milestone.go
@@ -3,32 +3,36 @@ package writer
 import (
 	"a-project-backend/db/model"
 	"a-project-backend/db/query"
+	"a-project-backend/svc/pkg/domain/model/milestone"
 	"errors"
+	"fmt"
+	"gorm.io/gorm"
 )
 
 type Milestone struct {
-	db *query.Query
+	gormDB *gorm.DB
+	db     *query.Query
 }
 
-func NewMilestone(db *query.Query) *Milestone {
-	return &Milestone{db: db}
+func NewMilestone(db *gorm.DB) *Milestone {
+	return &Milestone{db: query.Use(db), gormDB: db}
 }
 
-func (m Milestone) Create(target *model.Milestone) error {
+func (m Milestone) Create(target *milestone.Milestone) error {
 	if target == nil {
 		return errors.New("milestone is nil")
 	}
-	if target.MilestoneID != "" {
+	if target.ID != "" {
 		return errors.New("milestone id is not empty")
 	}
 	dbModel := model.Milestone{
-		MilestoneID: target.MilestoneID,
-		UserID:      target.UserID,
+		MilestoneID: string(target.ID),
+		UserID:      string(target.UserID),
 		Title:       target.Title,
 		Content:     target.Content,
-		ImageHash:   target.ImageHash,
-		BeginDate:   target.BeginDate,
-		FinishDate:  target.FinishDate,
+		ImageHash:   string(target.ImageID),
+		BeginDate:   int32(target.BeginDate),
+		FinishDate:  int32(target.FinishDate),
 	}
 	if err := m.db.Milestone.Create(&dbModel); err != nil {
 		return err
@@ -36,21 +40,35 @@ func (m Milestone) Create(target *model.Milestone) error {
 	return nil
 }
 
-func (m Milestone) Update(target model.Milestone) error {
-	if target.MilestoneID == "" {
+func (m Milestone) Update(target milestone.Milestone) error {
+	if target.ID == "" {
 		return errors.New("milestone id is empty")
 	}
 	dbModel := model.Milestone{
-		MilestoneID: target.MilestoneID,
-		UserID:      target.UserID,
+		MilestoneID: string(target.ID),
+		UserID:      string(target.UserID),
 		Title:       target.Title,
 		Content:     target.Content,
-		ImageHash:   target.ImageHash,
-		BeginDate:   target.BeginDate,
-		FinishDate:  target.FinishDate,
+		ImageHash:   string(target.ImageID),
+		BeginDate:   int32(target.BeginDate),
+		FinishDate:  int32(target.FinishDate),
 	}
 	if _, err := m.db.Milestone.Updates(&dbModel); err != nil {
 		return err
+	}
+	return nil
+}
+
+func (m Milestone) Delete(milestoneID []milestone.ID) error {
+	if len(milestoneID) == 0 {
+		return errors.New("milestone id is empty")
+	}
+	ids := make([]string, len(milestoneID))
+	for i, id := range milestoneID {
+		ids[i] = string(id)
+	}
+	if err := m.gormDB.Where("milestone_id IN ?", ids).Delete(&model.Milestone{}); err != nil {
+		return fmt.Errorf("failed to delete milestone: %w", err)
 	}
 	return nil
 }
