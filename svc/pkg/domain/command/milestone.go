e871f537f315da0f03ee75e6467664d09725b95f
Shion Ichikawa <shion1305@gmail.com>
2024-03-13 18:26:29 +0900

    🔥 command, query, model(partial)

--- svc/pkg/domain/command/milestone.go
+++
@@ -1,9 +0,0 @@
-package command
-
-import "a-project-backend/svc/pkg/domain/model/milestone"
-
-type Milestone interface {
-	Create(*milestone.Milestone) error
-	Update(milestone.Milestone) error
-	Delete([]milestone.ID) error
-}
