d6fde6fd97f71054c2ab4d2811f23bf808af635e
Shion Ichikawa <shion1305@gmail.com>
2024-03-15 00:13:15 +0900

    ✨ firebase auth compatible auth middleware

--- svc/pkg/middleware/auth.go
+++ svc/pkg/middleware/auth.go
@@ -2,9 +2,12 @@ package middleware
 
 import (
 	"a-project-backend/svc/pkg/domain/model/exception"
-	"a-project-backend/svc/pkg/domain/model/user"
 	"a-project-backend/svc/pkg/uc"
+	"encoding/json"
 	"github.com/gin-gonic/gin"
+	"io"
+	"log"
+	"net/http"
 )
 
 const (
@@ -12,12 +15,30 @@ const (
 )
 
 type auth struct {
-	loginUC uc.LoginUseCase
+	certString map[string]string
+	loginUC    uc.LoginUseCase
 }
 
 func NewAuth() auth {
+	resp, err := http.Get("https://www.googleapis.com/robot/v1/metadata/x509/securetoken@system.gserviceaccount.com")
+	if err != nil {
+		log.Fatalf("Failed to make a request: %v", err)
+	}
+	defer resp.Body.Close()
+
+	body, err := io.ReadAll(resp.Body)
+	if err != nil {
+		log.Fatalf("Failed to read the response body: %v", err)
+	}
+
+	var result map[string]string
+
+	if err = json.Unmarshal(body, &result); err != nil {
+		log.Fatalf("Failed to json unmarshal: %v", err)
+	}
 	return auth{
-		loginUC: uc.NewLoginUseCase(),
+		certString: result,
+		loginUC:    uc.NewLoginUseCase(),
 	}
 }
 
@@ -28,7 +49,7 @@ func (a auth) VerifyUser() gin.HandlerFunc {
 			c.AbortWithStatusJSON(400, gin.H{"error": err.Error()})
 			return
 		}
-		result, err := a.loginUC.Do(c, uc.LoginInput{JWT: user.JWT(jwt)})
+		result, err := a.loginUC.Do(c, uc.LoginInput{JWT: jwt})
 		if result == nil || err != nil {
 			c.AbortWithError(401, err)
 			return
--- svc/pkg/uc/login.go
+++ svc/pkg/uc/login.go
@@ -1,40 +1,81 @@
 package uc
 
 import (
-	"a-project-backend/pkg/config"
 	"a-project-backend/pkg/jwt"
-	"a-project-backend/svc/pkg/domain/model/user"
 	"context"
+	"encoding/base64"
+	"encoding/json"
+	"fmt"
+	"io"
+	"log"
+	"net/http"
+	"strings"
 )
 
 type LoginUseCase struct {
-	jwtSecret string
+	certString map[string]string
 }
 
 type LoginInput struct {
-	JWT user.JWT
+	JWT string
 }
 
 type LoginOutput struct {
-	UserID user.ID
+	UserID string
 }
 
 func NewLoginUseCase() LoginUseCase {
-	conf := config.Get()
-	if conf.Service.Authentication.JwtSecret == "" {
-		panic("jwt secret is not set")
+	resp, err := http.Get("https://www.googleapis.com/robot/v1/metadata/x509/securetoken@system.gserviceaccount.com")
+	if err != nil {
+		log.Fatalf("Failed to make a request: %v", err)
+	}
+	defer resp.Body.Close()
+
+	body, err := io.ReadAll(resp.Body)
+	if err != nil {
+		log.Fatalf("Failed to read the response body: %v", err)
+	}
+
+	var result map[string]string
+
+	if err = json.Unmarshal(body, &result); err != nil {
+		log.Fatalf("Failed to json unmarshal: %v", err)
 	}
 	return LoginUseCase{
-		jwtSecret: conf.Service.Authentication.JwtSecret,
+		certString: result,
 	}
 }
 
 func (uc LoginUseCase) Do(ctx context.Context, input LoginInput) (*LoginOutput, error) {
-	claims, err := jwt.Verify(string(input.JWT), uc.jwtSecret)
+	// JWTのヘッダを解析し署名に用いられている鍵を取得
+	parts := strings.Split(input.JWT, ".")
+
+	// decode the header
+	headerJson, err := base64.RawURLEncoding.DecodeString(parts[0])
+	if err != nil {
+		return nil, fmt.Errorf("error decoding JWT header: %v", err)
+	}
+
+	var header map[string]interface{}
+	err = json.Unmarshal(headerJson, &header)
+	if err != nil {
+		return nil, fmt.Errorf("error unmarshaling JWT header: %v", err)
+	}
+
+	kid := header["kid"].(string)
+	cert, ok := uc.certString[kid]
+	if !ok {
+		return nil, fmt.Errorf("kid not found: %v", kid)
+	}
+
+	claims, err := jwt.Verify(input.JWT, cert)
 	if err != nil {
 		return nil, err
 	}
+	if claims.Issuer != "https://securetoken.google.com/a-projcect-frontend" {
+		return nil, fmt.Errorf("invalid issuer: %v", claims.Issuer)
+	}
 	return &LoginOutput{
-		UserID: user.ID(claims.Id),
+		UserID: claims.Subject,
 	}, nil
 }
